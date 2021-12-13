505bbd6305e1631026b79e0c3318525cd97987ff
mengfanHu508 <2601477476@qq.com>
2021-12-12 17:37:26 -0800

    ejs success send

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://172.21.2.236:27017/190110910508');
 const express = require('express');
+const ejs = require('ejs');
 const app = express()
 const port = 5508
 
@@ -20,10 +21,17 @@ const boy = mongoose.model('boys', schema);
 // boy.find({name: 'shange'}, (err, data) => {console.log(data[0]._doc)});
 app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
-    res.send(req.query);
+    // res.send(req.query);
     console.log(req.query);
     const kitty = new boy({name: req.query.first,health:req.query.second});
     kitty.save()
+    // ejs.renderFile(filename,data,options,function(err,str){
+
+    // });
+    ejs.renderFile("result.html",{returnVal:"sucess"},function(err,str){
+        res.send(str);
+    });
+
 })
 app.listen(port,()=>{
     console.log("hmf508")
